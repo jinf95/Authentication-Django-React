f9894bb5399a1e3294e985583c7c31d4402798ce
Jinf95 <jinf95n@gmail.com>
2023-07-28 17:59:37 -0300

    Se agrego serializer, urls de app 'users' y vistas de registro y recuperacion de usuario

--- backend/authentication/users/models.py
+++ backend/authentication/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, Permis
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, password=None):
+    def create_user(self, first_name, last_name, email, password=None):
 
         if not email:
             raise ValueError("Users must have an email address")
@@ -23,7 +23,7 @@ class MyUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, first_name, last_name, password=None):
+    def create_superuser(self, first_name, last_name, email, password=None):
         user = self.create_user(
             first_name,
             last_name,
@@ -37,12 +37,12 @@ class MyUserManager(BaseUserManager):
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(
         max_length=255,
         unique=True,
     )
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
