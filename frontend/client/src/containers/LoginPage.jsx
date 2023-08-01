9cfa8e0d7effd11f4370248c314a3f160a790234
Jinf95 <jinf95n@gmail.com>
2023-08-01 18:05:03 -0300

    Se estructuro carpetas y archivos a utilizar, se incorporo react bootstrap, navbar y rutas con react-router-dom

---
+++ frontend/client/src/containers/LoginPage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const LoginPage = () => {
+  return (
+    <Layout title="Auth site | Login" content="Login page">
+      <h1>LoginPage</h1>
+    </Layout>
+  );
+};
+
+export default LoginPage;
---
+++ frontend/client/src/containers/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const RegisterPage = () => {
+  return (
+    <Layout title="Auth site | Register" content="Register page">
+      <h1>RegisterPage</h1>
+    </Layout>
+  );
+};
+
+export default RegisterPage;
